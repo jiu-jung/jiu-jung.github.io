2088861779d9a8def17508079c43c7e7ffa34baa
Jiwoo Jung <jiu.jung.cs@gmail.com>
2026-03-23 23:24:44 +0900

    ci: migrate blog deployment to Github Pages Actions

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "jiu's blog",
   description: "Hello, I'm jiu. Welcome to my blog!",
   author: "jiu",
-  siteUrl: "https://myblog.com",
+  siteUrl: "https://jiu-jung.github.io/",
   links: {
     github: "https://github.com/jiu-jung",
     linkedIn: "https://www.linkedin.com/in/jiwoo-jung-cs/",
@@ -12,13 +12,11 @@ module.exports = {
   // See https://giscus.app/
   giscus: {
     repo: "jiu-jung/jiu-jung.github.io",
-    repoId: "{YOUR_GITHUB_REPOSITORY_ID}",
-    category: "{YOUR_GITHUB_REPOSITORY_CATEGORY}",
-    categoryId: "{YOUR_GITHUB_REPOSITORY_CATEGORY_ID}",
+    repoId: "R_kgDORufRGA",
     mapping: "pathname",
     strict: "0",
     reactionsEnabled: "1",
     inputPosition: "bottom",
-    lang: "en",
+    lang: "ko",
   },
 }
